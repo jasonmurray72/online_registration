319a951e9febf788edc71b617444374e5358a17e
Jason Murray <jasonmurray72@gmail.com>
2015-11-19 13:38:05 -0500

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+	$mysql_hostname = "localhost";
+	$mysql_user = "username";
+	$mysql_password = "password";
+	$mysql_database = "dbname";
+	$prefix = "";
+	$conn = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+	mysql_select_db($mysql_database, $conn) or die("Could not select database");
+?>
